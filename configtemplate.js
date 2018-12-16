81ee8d34b92840d44e8fbb8cbff2daac798a558c
Jorgen <jorgen@janssonlee.com>
2018-12-16 23:02:36 +1100

    Working get-trakt-watchlist-node.js Added external config file for trakt credentials

---
+++ configtemplate.js
@@ -0,0 +1,16 @@
+// config.js
+// for now, use postman to generate/refresh codes
+
+const config = {
+ trakt: {
+   traktFriendID: "",
+   traktClientID: "",
+   traktClientSecret: "",
+   traktAccessToken: "",
+   traktCreatedAt: ,
+   traktAccessTokenExpiresIn: ,
+   traktRefreshToken: ""
+ }
+};
+
+module.exports = config;
